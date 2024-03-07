7682c7d38ec6e800d49ee3e537afb8c3716580ea
rakun256 <emreuslu256@gmail.com>
2024-03-07 12:32:08 +0300

    eklentiler

--- src/Pages/EventTeam/eventTeam.js
+++ src/Pages/EventTeam/eventTeam.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import TeamMember from './teamBox'; // Assuming TeamMember component is in the same directory
-import './eventTeam.css'
+import React, { useState, useEffect } from 'react';
+import TeamMember from './teamBox';
+import axios from 'axios';
+import './eventTeam.css';
 
 const OurTeam = () => {
+  const [teamData, setTeamData] = useState([]);
+
+  useEffect(() => {
+    const fetchTeamData = async () => {
+      try {
+        const response = await axios.get('https://api.bin.net.tr:8080/api/staff/getStaff');
+        setTeamData(response.data.data);
+      } catch (error) {
+        console.error('Error fetching team data:', error);
+      }
+    };
+
+    fetchTeamData();
+  }, []);
+
   return (
     <div className='event-team'>
       <h2 className='team-title'>EKİBİMİZ</h2>
       <div className='member-grid'>
-      <TeamMember />
-      <TeamMember />
-      <TeamMember />
-      <TeamMember />
-      <TeamMember />
-      <TeamMember />
+        {teamData.map((member, index) => (
+          <TeamMember key={index} member={member} />
+        ))}
       </div>
     </div>
   );
--- src/Pages/EventTeam/teamBox.js
+++ src/Pages/EventTeam/teamBox.js
@@ -1,32 +1,23 @@
-// TeamMember.js
-
 import React from 'react';
-import './teamBox.css'; // Import the CSS file
+import './teamBox.css';
 
-const TeamMember = () => {
-  const memberInfo = {
-    imageSrc: 'Images/ben.jpg',
-    memberName: 'Emre Uslu',
-    memberDepartment: 'Matematik Mühendisliği',
-    linkedinUrl: 'https://www.linkedin.com/in/emre-uslu-31a042291/',
-  };
-
+const TeamMember = ({ member }) => {
   const handleImageClick = () => {
-    window.location.href = memberInfo.linkedinUrl;
+    window.location.href = member.linkedin;
   };
 
   return (
     <div className="team-member-container">
       <div className="member-photo-container">
         <img
-          src={memberInfo.imageSrc}
-          alt={memberInfo.memberName}
+          src={member.photo.photoUrl}
+          alt={member.firstName + member.lastName}
           onClick={handleImageClick}
           className="member-photo"
         />
       </div>
-      <h3 className="member-name">{memberInfo.memberName}</h3>
-      <p className="member-department">{memberInfo.memberDepartment}</p>
+      <h3 className="member-name">{member.firstName}  {member.lastName}</h3>
+      <p className="member-department">{member.department}</p>
     </div>
   );
 };
