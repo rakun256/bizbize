0ebaf99d98a35bed1ca0515547b686949065138a
Kuzey Sınay <kuzeysinay@gmail.com>
2024-03-08 21:49:09 +0300

    fix

--- src/Pages/PreviousEvents/previousEvents.js
+++ src/Pages/PreviousEvents/previousEvents.js
@@ -2,20 +2,18 @@ import React, { useEffect, useState } from "react";
 import "./previousEvents.css";
 import PreviousEventBox from "./previousEventBox";
 import axios from "axios";
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 const PreviousEvents = () => {
   const [events, setEvents] = useState([]);
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()])
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()]);
 
   useEffect(() => {
     if (emblaApi) {
-      console.log(emblaApi.slideNodes()) // Access API
+      console.log(emblaApi.slideNodes()); // Access API
     }
-  }, [emblaApi])
-
+  }, [emblaApi]);
 
   useEffect(() => {
     const fetchEventsData = async () => {
@@ -32,49 +30,19 @@ const PreviousEvents = () => {
     fetchEventsData();
   }, []);
 
-  
   return (
     <div className='previous-events' id='prevent'>
       <h1 className='prevent-title'>ETKİNLİKLERİMİZ</h1>
       <div className='prevent-container'>
-
-
-      <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-      {events
-          .filter((event_) => event_.isActive === 0)
-          .map((event, index) => (
-            <PreviousEventBox event={event} index={index} />
-          ))}
-           {events
-          .filter((event_) => event_.isActive === 0)
-          .map((event, index) => (
-            <PreviousEventBox event={event} index={index} />
-          ))}
-           {events
-          .filter((event_) => event_.isActive === 0)
-          .map((event, index) => (
-            <PreviousEventBox event={event} index={index} />
-          ))}
-
-{events
-          .filter((event_) => event_.isActive === 0)
-          .map((event, index) => (
-            <PreviousEventBox event={event} index={index} />
-          ))}
-          {events
-          .filter((event_) => event_.isActive === 0)
-          .map((event, index) => (
-            <PreviousEventBox event={event} index={index} />
-          ))}
-          {events
-          .filter((event_) => event_.isActive === 0)
-          .map((event, index) => (
-            <PreviousEventBox event={event} index={index} />
-          ))}
-      </div>
-    </div>
-       
+        <div className='embla' ref={emblaRef}>
+          <div className='embla__container'>
+            {events
+              .filter((event_) => event_.isActive === 0)
+              .map((event, index) => (
+                <PreviousEventBox event={event} index={index} />
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
