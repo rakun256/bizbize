929d87976b4b55538ec1841b196b34ad0443bd67
Kuzey Sınay <kuzeysinay@gmail.com>
2024-03-10 16:36:25 +0300

    Multiple commit -add dots and buttons under the carousel -sort the events by the date before mapping -guest image arrangements

--- src/Pages/PreviousEvents/previousEvents.js
+++ src/Pages/PreviousEvents/previousEvents.js
@@ -2,12 +2,31 @@ import React, { useEffect, useState } from "react";
 import "./previousEvents.css";
 import PreviousEventBox from "./previousEventBox";
 import axios from "axios";
+
+import "./embla.css"
+
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from "./EmblaCarouselArrowButtons";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
 
 const PreviousEvents = () => {
   const [events, setEvents] = useState([]);
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
 
   useEffect(() => {
     if (emblaApi) {
@@ -34,13 +53,40 @@ const PreviousEvents = () => {
     <div className='previous-events' id='prevent'>
       <h1 className='prevent-title'>ETKİNLİKLERİMİZ</h1>
       <div className='prevent-container'>
-        <div className='embla' ref={emblaRef}>
-          <div className='embla__container'>
-            {events
-              .filter((event_) => event_.isActive === 0)
-              .map((event, index) => (
-                <PreviousEventBox event={event} index={index} />
+        <div className='embla' >
+          <div className='embla__viewport' ref={emblaRef}>
+            <div className='embla__container' ref={emblaRef}>
+              {events
+                .filter((event_) => event_.isActive === 0)
+                .sort((a, b) => new Date(b.date) - new Date(a.date))
+                .map((event, index) => (
+                  <PreviousEventBox event={event} index={index} />
+                ))}
+            </div>
+          </div>
+          <div className='embla__controls'>
+            <div className='embla__buttons'>
+              <PrevButton
+                onClick={onPrevButtonClick}
+                disabled={prevBtnDisabled}
+              />
+              <NextButton
+                onClick={onNextButtonClick}
+                disabled={nextBtnDisabled}
+              />
+            </div>
+
+            <div className='embla__dots'>
+              {scrollSnaps.map((_, index) => (
+                <DotButton
+                  key={index}
+                  onClick={() => onDotButtonClick(index)}
+                  className={"embla__dot".concat(
+                    index === selectedIndex ? " embla__dot--selected" : ""
+                  )}
+                />
               ))}
+            </div>
           </div>
         </div>
       </div>
