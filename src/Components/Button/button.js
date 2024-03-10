542d0597813c4d6cc23a145e93b4d78cd3c201f8
Emre Uslu <emreuslu256@gmail.com>
2024-03-10 19:01:03 +0300

    link düzeltildi

--- src/Components/Button/button.js
+++ src/Components/Button/button.js
@@ -4,7 +4,7 @@ import './button.css';
 
 const Button = () => {
   return (
-    <a className="ButtonContainer" href="https://linktr.ee/ytuskylab" target="_blank" rel="noopener noreferrer">
+    <a className="ButtonContainer" href="https://forms.office.com/r/6KEewxhZtH" target="_blank" rel="noopener noreferrer">
       Kayıt Ol!
     </a>
   );
