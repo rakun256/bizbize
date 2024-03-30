238ee257666a235f10edc0b3da010c819f6b92ed
Kuzey Sınay <kuzeysinay@gmail.com>
2024-03-30 04:07:36 +0300

    fix minus time left bug

--- src/Components/Countdown/countdown.js
+++ src/Components/Countdown/countdown.js
@@ -4,6 +4,9 @@ import "./countdown.css";
 const Countdown = ({ target }) => {
   const getTimeLeft = () => {
     const totalTimeLeft = target - new Date();
+    if (totalTimeLeft <= 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
     const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor(((totalTimeLeft / (1000 * 60 * 60)) % 24));
     const minutes = Math.floor((totalTimeLeft / (1000 * 60)) % 60);
@@ -29,6 +32,10 @@ const Countdown = ({ target }) => {
     minutes: "Dakika",
     seconds: "Saniye",
   };
+
+  if (timeLeft.days <= 0 && timeLeft.hours <= 0 && timeLeft.minutes <= 0 && timeLeft.seconds <= 0) {
+    return null; // If time left is below or equal to 0, return nothing
+  }
 
   return (
     <div className="countdown">
