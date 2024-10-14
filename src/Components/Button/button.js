aeb2d2e0e2988944c31d830b8f4f0c8dfcc28d89
Kuzey Sınay <69167006+kuzeysinay@users.noreply.github.com>
2024-10-14 20:47:33 +0300

    update microsoft form links with recent one

--- src/Components/Button/button.js
+++ src/Components/Button/button.js
@@ -4,7 +4,7 @@ import './button.css';
 
 const Button = () => {
   return (
-    <a className="ButtonContainer" href="https://forms.office.com/r/xidBuHgMwy" target="_blank" rel="noopener noreferrer">
+    <a className="ButtonContainer" href="https://forms.office.com/pages/responsepage.aspx?id=CClghVvhukORSDi8dzqBbo7p7UNrw4dGn2QLKKVhM5VUOVVFWlNIMldZWDZDSVYzVDFDUjZQMTVOTS4u&fbclid=PAZXh0bgNhZW0CMTEAAaZiimpmUoajw3avGm0Knej2dV2XMrrSpNPrOIX8RJDjk4f6g6xmV5WZnyI_aem_5wWwWpYirOzgOA2KTGi_Ww&route=shorturl" target="_blank" rel="noopener noreferrer">
       Kayıt Ol!
     </a>
   );
