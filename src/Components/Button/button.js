b7d24e6a3d2dd10fbbde32edeeed2c12508374b8
Kuzey Sınay <69167006+kuzeysinay@users.noreply.github.com>
2024-10-29 17:08:02 +0300

    recent form url

--- src/Components/Button/button.js
+++ src/Components/Button/button.js
@@ -4,7 +4,7 @@ import './button.css';
 
 const Button = () => {
   return (
-    <a className="ButtonContainer" href="https://forms.office.com/pages/responsepage.aspx?id=CClghVvhukORSDi8dzqBbo7p7UNrw4dGn2QLKKVhM5VUOVVFWlNIMldZWDZDSVYzVDFDUjZQMTVOTS4u&fbclid=PAZXh0bgNhZW0CMTEAAaZiimpmUoajw3avGm0Knej2dV2XMrrSpNPrOIX8RJDjk4f6g6xmV5WZnyI_aem_5wWwWpYirOzgOA2KTGi_Ww&route=shorturl" target="_blank" rel="noopener noreferrer">
+    <a className="ButtonContainer" href="https://forms.office.com/pages/responsepage.aspx?id=CClghVvhukORSDi8dzqBbo7p7UNrw4dGn2QLKKVhM5VUOERBTU5TRFFXWU9KRlZOMFRKSlJTS1lIQy4u&fbclid=PAZXh0bgNhZW0CMTEAAaZwCMMWZmAyFz7SdxYjFIPQhFH8gRUke5AMlpBi0bMABYjJCeldXwQStYA_aem_KHj9KgOltAcTbXhQFtoz5A&route=shorturl" target="_blank" rel="noopener noreferrer">
       Kayıt Ol!
     </a>
   );
