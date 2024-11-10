df690b95c0200ec17caa8b53b6dadcab46fa4c93
Kuzey Sınay <69167006+kuzeysinay@users.noreply.github.com>
2024-11-10 13:41:31 +0300

    Update button.js

--- src/Components/Button/button.js
+++ src/Components/Button/button.js
@@ -4,7 +4,7 @@ import './button.css';
 
 const Button = () => {
   return (
-    <a className="ButtonContainer" href="https://forms.office.com/pages/responsepage.aspx?id=CClghVvhukORSDi8dzqBbo7p7UNrw4dGn2QLKKVhM5VUOERBTU5TRFFXWU9KRlZOMFRKSlJTS1lIQy4u&fbclid=PAZXh0bgNhZW0CMTEAAaZwCMMWZmAyFz7SdxYjFIPQhFH8gRUke5AMlpBi0bMABYjJCeldXwQStYA_aem_KHj9KgOltAcTbXhQFtoz5A&route=shorturl" target="_blank" rel="noopener noreferrer">
+    <a className="ButtonContainer" href="https://forms.office.com/r/kGzHxFnn2v" target="_blank" rel="noopener noreferrer">
       Kayıt Ol!
     </a>
   );
