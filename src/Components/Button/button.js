729d35ba1d9ce56d876b750e3e6c9a6f88fe4be1
Kuzey Sınay <69167006+kuzeysinay@users.noreply.github.com>
2024-11-22 21:34:27 +0300

    Update button.js

--- src/Components/Button/button.js
+++ src/Components/Button/button.js
@@ -4,7 +4,7 @@ import './button.css';
 
 const Button = () => {
   return (
-    <a className="ButtonContainer" href="https://forms.office.com/r/kGzHxFnn2v" target="_blank" rel="noopener noreferrer">
+    <a className="ButtonContainer" href="https://forms.office.com/r/Ayjk3AfAM2" target="_blank" rel="noopener noreferrer">
       Kayıt Ol!
     </a>
   );
